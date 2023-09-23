a0f4b482a54d7c8a08f4d5e63079dca25e48ac02
煎人寿 <771909584@qq.com>
2023-09-23 10:35:25 +0800

    add post Content dao and model

--- dao/dao.go
+++ dao/dao.go
@@ -12,9 +12,28 @@ import (
 type Manager interface {
 	AddUser(user *models.User)
 	Login(username string) models.User
+
+	AddPost(Post *models.Post)
+	GetAllPost() []models.Post
+	GetPost(pid int) models.Post
 }
+
 type manager struct {
 	db *gorm.DB
+}
+
+func (mgr *manager) AddPost(post *models.Post) {
+	mgr.db.Create(post)
+}
+func (mgr *manager) GetAllPost() []models.Post {
+	var posts = make([]models.Post, 10)
+	mgr.db.Find(&posts)
+	return posts
+}
+func (mgr *manager) GetPost(pid int) models.Post {
+	var post models.Post
+	mgr.db.First(&post, pid)
+	return post
 }
 
 func (m manager) AddUser(user *models.User) {
